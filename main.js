caf5223227fbd129f8b8a05a6a1a1333941b09d0
TanmayShakya <99342081+TanmayShakya@users.noreply.github.com>
2023-05-17 13:40:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var prediction = "";
+
+Webcam.set({
+    height: 350,
+    width: 350,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function takeSnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+    });
+
+}
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1eWG_URbY/model.json", modelReady);
+
+function modelReady() {
+    console.log("ModelReady", ml5.version)
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(result);
+        prediction = result[0].label;
+        document.getElementById("resultArray").innerHTML = prediction;
+
+        if(result[0].label == "Victory") {
+            document.getElementById("emoji5").innerHTML = "&#9996;";
+        }
+
+        if (result[0].label == "Best") {
+            document.getElementById("emoji5").innerHTML = "👍";
+        }
+
+        if(result[0].label == "Amazing") {
+            document.getElementById("emoji5").innerHTML = "👌";
+        }
+
+        
+    }
+}
+
